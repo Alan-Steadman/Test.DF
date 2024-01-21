077fbe68f06d4f6dcca069571f8297a4a5561de0
Alan Steadman <alan.steadman@gmail.com>
2024-01-21 13:59:37 +0000

    Created Person model

---
+++ Test.DF/Models/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace Test.DF.Models;
+
+public class Person
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateOnly Dob { get; set; }
+
+    private Person(string firstName, string lastName, DateOnly dob)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Dob = dob;
+    }
+
+    public static Person Create(string fullName, DateOnly dob)
+    {
+        if (!fullName.Contains(" "))
+        {
+            throw new ArgumentException("Full name must contain a first name and a last name seperated by a space");
+        }
+
+        if (dob >= DateOnly.FromDateTime(DateTime.Now))
+        {
+            throw new ArgumentException("Date of birth cannot be in the future");
+        }
+
+        var firstName = fullName.Substring(0, fullName.IndexOf(" "));
+        var lastName = fullName.Substring(fullName.IndexOf(" ") + 1); // Ignoring multiple spaces
+
+        return new Person(firstName, lastName, dob);
+    }
+
+}
